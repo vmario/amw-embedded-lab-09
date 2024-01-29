4a366327374757d605b99b4cb38c913094ba7a81
Mariusz Chilmon <vmario1986@gmail.com>
2024-01-29 18:26:52 +0100

    Draft

--- main.cpp
+++ main.cpp
@@ -5,26 +5,37 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-volatile bool falstart;
-
+#if 1
 /**
- * Preskaler Timer/Counter1.
+ * Czy był falstart?
  */
-constexpr uint8_t TIMER1_PRESCALER = _BV(CS12) | _BV(CS10);
-
-/**
- * Konfiguracja Timer/Counter1.
- */
-constexpr uint8_t TIMER1_MODE = _BV(WGM12);
+volatile bool falseStart;
+#endif
 
 /**
  * Obsługa przerwania komparatora Timer/Counter1.
  */
 ISR(TIMER1_OVF_vect)
 {
+#if 1
 	TCCR1B = 0;
 	TCNT1 = 0xFFFF;
-	falstart = true;
+	falseStart = true;
+#endif
+}
+
+/**
+ * Odliczany czas.
+ *
+ * @param Wartość aktualnie odliczonego czasu w milisekundach.
+ */
+uint16_t currentTime()
+{
+#if 1
+	return 1.0 * TCNT1 / (16'000'000 / 1024) * 1000;
+#else
+	return 0;
+#endif
 }
 
 /**
@@ -32,40 +43,55 @@ ISR(TIMER1_OVF_vect)
  */
 ISR(TIMER0_OVF_vect)
 {
-	display.print(1.0 * TCNT1 / (16'000'000 / 1024) * 1000, 3);
+	display.print(currentTime(), 3);
 	display.refresh();
 }
 
+/**
+ * Przerwanie od przycisku.
+ */
 ISR(PCINT1_vect)
 {
+#if 1
 	TCCR1B = 0;
-	falstart = true;
+	falseStart = true;
+#endif
 }
 
-void buzzerOn()
-{
-	DDRB |= _BV(5);
-	//DDRD |= _BV(3);
-}
-
-void buzzerOff()
-{
-	DDRB &= ~_BV(5);
-	//DDRD &= ~_BV(3);
-}
-
+/**
+ * Pętla główna.
+ */
 void mainLoop()
 {
+#if 1
 	_delay_ms(5'000);
-	if (falstart) {
-		falstart = false;
+	if (falseStart) {
+		falseStart = false;
 		return;
 	}
 	TCNT1 = 0;
-	buzzerOn();
-	TCCR1B = TIMER1_PRESCALER;
+
+	DDRB |= _BV(5);
+	//DDRD |= _BV(3);
+
+	TCCR1B = _BV(CS12) | _BV(CS10);
 	_delay_ms(20);
-	buzzerOff();
+
+	DDRB &= ~_BV(5);
+	//DDRD &= ~_BV(3);
+#endif
+}
+
+/**
+ * Inicjalizacja przerwań.
+ */
+void interruptInitialize()
+{
+#if 1
+	TIMSK1 = _BV(TOIE1);
+	PCICR |= _BV(PCIE1);
+	PCMSK1 |= _BV(PCINT9);
+#endif
 }
 
 /**
@@ -75,10 +101,7 @@ int main()
 {
 	shifter.initialize();
 	timer.initialize();
-
-	PCICR |= _BV(PCIE1);
-	PCMSK1 |= _BV(PCINT9);
-	TIMSK1 = _BV(TOIE1);
+	interruptInitialize();
 
 	sei();
 

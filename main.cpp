101b544493b2afc2b0eb470a5253412548c1682a
Mariusz Chilmon <vmario1986@gmail.com>
2024-01-29 22:20:58 +0100

    Clean

--- main.cpp
+++ main.cpp
@@ -5,23 +5,11 @@
 #include <avr/interrupt.h>
 #include <util/delay.h>
 
-#if 1
-/**
- * Czy był falstart?
- */
-volatile bool falseStart;
-#endif
-
 /**
  * Obsługa przerwania przepełnienia Timer/Counter1.
  */
 ISR(TIMER1_OVF_vect)
 {
-#if 1
-	TCCR1B = 0;
-	TCNT1 = 0xFFFF;
-	falseStart = true;
-#endif
 }
 
 /**
@@ -31,12 +19,6 @@ ISR(TIMER1_OVF_vect)
  */
 uint16_t currentTime()
 {
-#if 1
-	return 1.0 * TCNT1 / (16'000'000 / 1024) * 1000;
-#endif
-#if 1
-	return TCNT1;
-#endif
 	return 8888;
 }
 
@@ -54,10 +36,6 @@ ISR(TIMER0_OVF_vect)
  */
 ISR(PCINT1_vect)
 {
-#if 1
-	TCCR1B = 0;
-	falseStart = true;
-#endif
 }
 
 /**
@@ -65,35 +43,6 @@ ISR(PCINT1_vect)
  */
 void mainLoop()
 {
-#if 1
-	_delay_ms(5'000);
-#endif
-
-#if 1
-	if (falseStart) {
-		falseStart = false;
-		return;
-	}
-#endif
-
-#if 1
-	TCNT1 = 0;
-#endif
-
-#if 1
-	DDRB |= _BV(5);
-	//DDRD |= _BV(3);
-#endif
-
-#if 1
-	TCCR1B = _BV(CS12) | _BV(CS10);
-#endif
-
-#if 1
-	_delay_ms(20);
-	DDRB &= ~_BV(5);
-	//DDRD &= ~_BV(3);
-#endif
 }
 
 /**
@@ -101,13 +50,6 @@ void mainLoop()
  */
 void interruptsInitialize()
 {
-#if 1
-	PCMSK1 |= _BV(PCINT9);
-	PCICR |= _BV(PCIE1);
-#endif
-#if 1
-	TIMSK1 = _BV(TOIE1);
-#endif
 }
 
 /**
